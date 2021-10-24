5070af8b8695102ae2f998f830cf5c99a6f3a4e9
Ray Colt <57169235+RayColt@users.noreply.github.com>
2021-10-24 15:41:47 +0200

    Add project files.

---
+++ system32-shortcut/system32-shortcut.cpp
@@ -0,0 +1,17 @@
+#include <cstdlib>
+
+/*
+* system32-shortcut 
+* 
+* @author Ray Colt <ray_colt@pentagon.mil> <ray_colt@microsoft.com>
+* @copyright Copyright (c) 2021 Ray Colt
+* 
+* for example using it for Midnight Commander for Windows
+* compile this as mc.exe
+* now you can run the command mc in your console (for mc like in Linux)
+* anywhere in you Windows
+*/
+int main()
+{ 
+    system("\"\"C:\\Program Files (x86)\\\Midnight Commander\\\mc.exe\"");
+}
