0c9d60acc4267112858592321aa5b8adcd6bbab3
Ray Colt <57169235+RayColt@users.noreply.github.com>
2021-10-24 20:49:47 +0200

    upd

--- system32-shortcut/system32-shortcut.cpp
+++ system32-shortcut/system32-shortcut.cpp
@@ -7,7 +7,7 @@
 * @copyright Copyright (c) 2021 Ray Colt
 * 
 * for example using it for Midnight Commander for Windows
-* compile this as mc.exe
+* compile this as mc.exe place it in C:\Windows\System32
 * now you can run the command mc in your console (for mc like in Linux)
 * anywhere in you Windows
 */
