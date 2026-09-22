c00785fdfd058c523156c1524143117f0eaae2bb
Ray Colt <57169235+RayColt@users.noreply.github.com>
2026-09-22 09:19:30 +0200

    Update copyright year in system32-shortcut.cpp
    Updated copyright year from 2021 to 2021 - 2026.

--- system32-shortcut/system32-shortcut.cpp
+++ system32-shortcut/system32-shortcut.cpp
@@ -3,10 +3,14 @@
 * system32-shortcut 
 * 
 * @author Ray Colt <ray_colt@colt.com> <ray_colt@microsoft.com>
-* @copyright Copyright (c) 2021 Ray Colt
+* @copyright Copyright (c) 2021 - 2026 Ray Colt
 * 
 */
 int main()
 { 
     system("\"\"C:\\Program Files (x86)\\\your-program\\\your-program.exe\"");
+    //example:
+    //system("\"\"C:\\Windows\\\System32\\\wsl.exe\"");
+    // compile as deb.exe and place in system32
+    // Windows key + R deb
 }
