0a1280e2830addd4ca8b8f6f5da1ada5a5f58e69
Ray Colt <57169235+RayColt@users.noreply.github.com>
2026-09-22 09:32:19 +0200

    Clarify WSL example in system32-shortcut.cpp
    Updated comment to clarify example for opening WSL.

--- system32-shortcut/system32-shortcut.cpp
+++ system32-shortcut/system32-shortcut.cpp
@@ -9,7 +9,7 @@
 int main()
 { 
     system("\"\"C:\\Program Files (x86)\\\your-program\\\your-program.exe\"");
-    // example for opening wsl debian:
+    // example for opening C:\Windows\System32\wsl.exe debian:
     // system("\"\"C:\\Windows\\\System32\\\wsl.exe\"");
     // compile as deb.exe and place in system32
     // Windows key + R deb
